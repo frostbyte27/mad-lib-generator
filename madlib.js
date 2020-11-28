0590af95c24576b92bcbe2ee29001980c5d5a42c
Emma Kelley <alteremma88@gmail.com>
2020-11-27 17:49:19 -0800

    Create the madlib.js Javascript file with basic components

---
+++ madlib.js
@@ -0,0 +1,29 @@
+/***
+ * Mad Lib Generator
+ * Randomly generates a mad-lib style message
+ * By selecting a template and filler words
+ */
+
+//Mad Lib Templates
+const templates = [];
+
+//Each noun should include singular and plural version
+const nouns = [];
+//Each verb should include past, present and future tense
+const verbs = [];
+//Just adjectives
+const adjectives = [];
+
+
+/**
+ * Builds and returns a randomly generated Mad-Lib
+ */
+function generateMadLib(){
+
+
+}
+
+
+
+//Primary script
+console.log('Welcome to the Mad Lib Program');
